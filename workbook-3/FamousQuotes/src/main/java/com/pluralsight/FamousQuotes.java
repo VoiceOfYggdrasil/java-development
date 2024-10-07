b66087132ba1f617419746be717b1789a75ae065
VoiceOfYggdrasil <aaron.schmitt8@gmail.com>
2024-10-07 15:53:27 -0400

    Add workbook 3

---
+++ workbook-3/FamousQuotes/src/main/java/com/pluralsight/FamousQuotes.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class FamousQuotes {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] quotes = new String[10];
+
+        quotes[1] = "Is it better to be born good? Or to overcome one's evil nature?";
+        quotes[2] = "Unconditional. Inseparable.";
+        quotes[3] = "I am not the villain of this story. I do what I do because there is no choice.";
+        quotes[4] = "Protocol 3: Protect The Pilot.";
+        quotes[5] = "";
+
+    }
+}
