a1ceafd89a07f1caa4de16ada9bf848107b7b2a8
Aaron Schmitt <aaron.schmitt8@gmail.com>
2024-10-08 13:40:49 -0400

    Finished First Payroll Exercise

---
+++ workbook-3/PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.io.*;
+
+public class PayrollCalculator {
+    public static void main(String[] args) throws IOException {
+
+        String fileName = "employees.csv";
+
+        try {
+            BufferedReader buffReader = new BufferedReader(new FileReader(fileName));
+            String input;
+            while ((input = buffReader.readLine()) != null) {
+                String[] tokens = input.split("\\|");
+                int id = Integer.parseInt(tokens[0]);
+                String name = tokens[1];
+                double hoursWorked = Double.parseDouble(tokens[2]);
+                double payRate = Double.parseDouble(tokens[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+
+                System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f%n",
+                        employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+
+            }
+            buffReader.close();
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + fileName);
+        }
+    }
+}
