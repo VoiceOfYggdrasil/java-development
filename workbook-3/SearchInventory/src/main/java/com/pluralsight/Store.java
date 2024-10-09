d0914c1b825c542e538f590bd0ac0937d4157206
Aaron Schmitt <aaron.schmitt8@gmail.com>
2024-10-09 13:47:29 -0400

    Got SearchInventory to run

--- workbook-3/SearchInventory/src/main/java/com/pluralsight/Store.java
+++ workbook-3/SearchInventory/src/main/java/com/pluralsight/Store.java
@@ -13,7 +13,7 @@ public class Store {
 
         for (int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
-            System.out.printf("id: %d %s - Price: $%.2f",
+            System.out.printf("id: %d %s - Price: $%.2f\n",
                     p.getId(), p.getName(), p.getPrice());
         }
     }
@@ -21,11 +21,11 @@ public class Store {
     public static ArrayList<Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<Product>();
 
-        inventory.add(15043, "Playstation 5", 500.00);
-        inventory.add(60547, "Nikon D3500", 600.00);
-        inventory.add(33023, "Lenovo Ideapad", 800.00);
-        inventory.add(92164, "Samsung S22", 1000.00);
-        inventory.add(50038, "Sennheiser HD590", 250.00);
+        inventory.add(new Product(15043, "Playstation 5", 500.00));
+        inventory.add(new Product(60547, "Nikon D3500", 600.00));
+        inventory.add(new Product(33023, "Lenovo Ideapad", 800.00));
+        inventory.add(new Product(92164, "Samsung S22", 1000.00));
+        inventory.add(new Product(50038, "Sennheiser HD590", 250.00));
 
         return inventory;
     }
